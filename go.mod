23afbb7a77eb2ab982bcc429a3be5532734cebbe
dc-ops <darshan.chaluvaraju.github@gmail.com>
2024-11-03 17:20:59 +0530

    Adding gshep bark sound

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DC-Ops/puppy
 
 go 1.23.2
+
+require github.com/DC-Ops/gshep v0.0.0-20241103114635-6808d9d9e927 // indirect
--- puppy.go
+++ puppy.go
@@ -1,6 +1,15 @@
 package puppy
 
-func Bark() string {
+import (
+	"github.com/DC-Ops/gshep"
+)
+
+func BasicBark() string {
 	sound := "Woof!"
+	return sound
+}
+
+func GshepBark() string {
+	sound := gshep.GshepBark()
 	return sound
 }
